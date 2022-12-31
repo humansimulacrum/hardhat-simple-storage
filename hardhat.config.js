e99ea7938dfe1d3977eea24bde5c6bddb9b793f1
Mykhailo Benych <misha.benych@gmail.com>
2022-12-31 15:30:39 +0200

    added deployment and verification on etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+require('@nomiclabs/hardhat-etherscan');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.8",
+	defaultNetwork: 'hardhat',
+	networks: {
+		goerli: {
+			url: process.env.GOERLI_RPC_URL,
+			accounts: [process.env.GOERLI_PRIVATE_KEY],
+			chainId: 5,
+		},
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY,
+	},
+	solidity: '0.8.8',
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,40 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, run, network } = require('hardhat');
+require('dotenv').config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+	const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
+	console.log('Deploying contract...');
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+	const simpleStorage = await SimpleStorageFactory.deploy();
+	await simpleStorage.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	console.log(`Deployed contract to ${simpleStorage.address}`);
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	if (network.chainId !== 31337 && process.env.ETHERSCAN_API_KEY) {
+		await simpleStorage.deployTransaction.wait(6);
+		await verify(simpleStorage.address, []);
+	}
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, args) {
+	console.log('Verifying contract...');
+	try {
+		await run('verify:verify', {
+			address: contractAddress,
+			constructorArguments: args,
+		});
+	} catch (error) {
+		if (e.message.toLowerCase().includes('already verified')) {
+			console.log('Already verified');
+		} else {
+			console.error(e);
+		}
+	}
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	});
